f428ba9ad7fd32fd6b084976529200187621ef49
Abdul Miah <abdulmiah@Abduls-MacBook-Air.local>
2014-03-03 18:49:28 -0500

    master initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Digidex::Application.config.secret_key_base = '767ddb089bd006ef4865fbf66293679d33dc9d5b2dc139b00789145ec791cf4f3bd83563127b7e01da9870a3198e498d7b2283b18e4d0dca224aae0505f1367d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Digidex::Application.config.session_store :cookie_store, key: '_digidex_session'
