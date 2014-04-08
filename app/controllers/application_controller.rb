927f8d36eb23a4be44cc7001d3b5de31d730d533
Abdul Miah <abdulmiah@Abduls-MacBook-Air.local>
2014-04-07 21:57:12 -0400

    master edit layouts for login screens

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :configure_permitted_parameters, if: :devise_controller?
     
-  layout :layout
+  # layout :layout
 
   def after_sign_in_path_for(resource)
     cards_path
@@ -12,12 +12,12 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def layout
-    # only turn it off for login pages:
-    is_a?(Devise::SessionsController) ? false : "application"
-    # or turn layout off for every devise controller:
-    !devise_controller? && "application"
-  end
+  # def layout
+  #   # only turn it off for login pages:
+  #   is_a?(Devise::SessionsController) ? false : "application"
+  #   # or turn layout off for every devise controller:
+  #   !devise_controller? && "application"
+  # end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:account_update) do |u|
